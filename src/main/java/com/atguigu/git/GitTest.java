0e68a48ad934516d064075914d70bfd9224b8986
Tonkey <1480056419@qq.com>
2022-05-07 09:55:12 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @ProjectName: atguigu
+ * @Package: com.atguigu.git
+ * @ClassName: GitTest
+ * @Author: [Lucky Star]
+ * @Date: 2022/5/7 9:50
+ * @Version: V1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
